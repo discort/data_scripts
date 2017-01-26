c2ff260bd97dcd6d4446762f90bd95575f9330a2
discort <odiscort@gmail.com>
2017-01-26 10:57:47 +0200

    added requirements

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from oauth2client import GOOGLE_TOKEN_URI
 
 
-GOOGLE_API_KEY = ""
-GOOGLE_CLIENT_ID = ""
-GOOGLE_CLIENT_SECRET = ""
+GOOGLE_API_KEY = "AIzaSyBTdPz6FrXshZdXXLcmLGdbJQLImbV8TiU"
+GOOGLE_CLIENT_ID = "739526417457-9jgpod4bg7h5umm4fehu0kh0oo4n3c15.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "i99lJkRiuQYAVgGaSW--XK_U"
+GOOGLE_REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
